c4fcce958bb57415a9aa2bba0d820574ad1ca8c8
joelcmk <57118300+joelcmk@users.noreply.github.com>
2021-10-27 18:59:15 -0500

    Update submitform.js

--- src/components/submitform.js
+++ src/components/submitform.js
@@ -1,62 +1,89 @@
-import React, { useState } from 'react';
-import { Button, Form, Container, Header } from 'semantic-ui-react';
 import axios from 'axios';
+import React, { Component, useState } from 'react';
+import { Button, Form, Container, Header } from 'semantic-ui-react';
+// import axios from 'axios';
 
-const SubmitForm = () => {
+import { env } from '../utils/EnvironmentalVariables';
 
-  const [timeStamp, setTimeStamp] = useState(new Date());
-  const [projectIdea, setProjectIdea] = useState('');
-  const [targetAudience, setTargetAudience] = useState('');
-  const [notes, setNotes] = useState('');
-  const [tags, setTags] = useState('');
+export default class SubmitForm extends Component {
+  constructor(props) {
+    super(props)
 
-  const submitHandler = (e) => {
-    e.preventDefault();
-    let currentTime = new Date().toLocaleDateString('en-US');
-    setTimeStamp({ timeStamp: currentTime });
-    const ideaInfo = {
-      timeStamp: timeStamp,
-      projectIdea: projectIdea,
-      targetAudience: targetAudience,
-      notes: notes,
-      tags: tags,
-    }
-    axios
-      .post('https://sheet.best/api/sheets/80b3b2ab-831f-4049-a81a-0713020b673b', ideaInfo)
-      .then(response => {
-        console.log(response)
-      })
-    setTimeStamp(new Date());
-    setProjectIdea('');
-    setTargetAudience('');
-    setNotes('');
-    setTags('')
+    this.state = {
+      ideaName: '',
+      tagline: '',
+      description: '',
+      targetAudience: '',
+      hourCommitmentMin: 0,
+      hourCommitmentMax: 0,
+      skills: [
+        { skill: 'Web Development' },
+        { skill: 'AI / ML' },
+      ],
+      openPositions: [
+        {
+          title: 'Developer, UX/UI',
+          description: 'Lots of programming',
+          isHidden: false
+        }
+      ],
+      author: '1',
+      difficultyLevel: 'Beginner',
+      // timeStamp: new Date(),
+    };
   }
 
-  return (
-    <Container fluid className='container' id='formWrapper'>
-      <Header as='h1'>Submit your idea!!!</Header>
-      <Form id='survey-form' className='form'>
-        <Form.Field>
-          <label>What project idea?</label>
-          <input required type='text' name='projectIdea' value={projectIdea} onChange={e => setProjectIdea(e.target.value)}></input>
-        </Form.Field>
-        <Form.Field>
-          <label>Who's is the target audience for your idea?</label>
-          <input required type='text' name='targetAudience' value={targetAudience} onChange={e => setTargetAudience(e.target.value)}></input>
-        </Form.Field>
-        <Form.Field>
-          <label>Notes</label>
-          <input type='text' name='notes' value={notes} onChange={e => setNotes(e.target.value)}></input>
-        </Form.Field>
-        <Form.Field>
-          <label>Tags</label>
-          <input type='text' name='tags' value={tags} onChange={e => setTags(e.target.value)}></input>
-        </Form.Field>
-        <Button color='blue' type='submit' onClick={submitHandler}>Submit</Button>
-      </Form>
-    </Container>
-  )
+  changeHandler = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  submitHandler = e => {
+    e.preventDefault();
+    // i don't think we need the date stuff?
+    // get request of test posts still have a date
+    // let currentTime =  new Date().toLocaleDateString('en-US');
+    // this.setState({timeStamp: currentTime});
+
+    axios.post(`${env().STRAPI_URL}/idea-cards/`, this.state)
+      .then(response => {
+        console.log(response);
+      });
+
+    this.setState({
+      ideaName: '',
+      targetAudience: '',
+      description: '',
+      tagline: '',
+    })
+
+  };
+
+  render() {
+    const { ideaName, targetAudience, description, tagline } = this.state;
+    return (
+      <Container fluid className='container' id='formWrapper'>
+        <Header as='h1'>Submit your idea!!!</Header>
+        <Form id='survey-form' className='form' method='post' onSubmit={this.submitHandler}>
+          <Form.Field>
+            <label>What project idea?</label>
+            <input required type='text' name='ideaName' value={ideaName} onChange={this.changeHandler}></input>
+          </Form.Field>
+          <Form.Field>
+            <label>Who's is the target audience for your idea?</label>
+            <input required type='text' name='targetAudience' value={targetAudience} onChange={this.changeHandler}></input>
+          </Form.Field>
+          <Form.Field>
+            <label>Notes</label>
+            <input type='text' name='description' value={description} onChange={this.changeHandler}></input>
+          </Form.Field>
+          <Form.Field>
+            <label>Tags</label>
+            <input type='text' name='tagline' value={tagline} onChange={this.changeHandler}></input>
+          </Form.Field>
+          <Button color='blue' type='submit'>Submit</Button>
+        </Form>
+      </Container>
+    )
+  }
 }
 
-export default SubmitForm;
